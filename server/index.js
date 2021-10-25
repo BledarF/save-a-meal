025b9aa069c431041e7ad06fc1897033d31d7ec3
Adnan Gondal <adnangondal1@hotmail.com>
2021-10-25 17:16:32 +0100

    initial setup

---
+++ server/index.js
@@ -0,0 +1,20 @@
+// server/index.js
+
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
