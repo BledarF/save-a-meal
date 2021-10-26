8ba67f4bd2d6731a7d2231c0ec620bde096b21ac
Sohail <trainee.sohail.saiedi@sigmalabs.zyx>
2021-10-26 12:00:28 +0100

    React Router, NavBar, Components setup

---
+++ client/src/Components/Navbar/Button.js
@@ -0,0 +1,5 @@
+import { useEffect, useState } from "react";
+
+function Button() {}
+
+export default Button;
---
+++ client/src/Components/Register/Register.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+function Register() {
+  return (
+    <div>
+      <h1>Register Component</h1>
+    </div>
+  );
+}
+
+export default Register;
