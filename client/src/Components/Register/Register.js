c44fcf9278d8ffbdbc0e5bab1725a949881bee6b
Adnan Gondal <adnangondal1@hotmail.com>
2021-10-26 15:38:10 +0100

    added modal user register

--- client/src/Components/Register/Register.js
+++ client/src/Components/Register/Register.js
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
+import UserModal from "./UserModal";
 
 function Register() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="flex flex-row flex-wrap justify-items-stretch my-8 h-screen">
       <div
+        onClick={() => {
+          setShowModal(true);
+        }}
         className="flex-initial bg-user-login  w-1/2 h-5/6 bg-cover  
       border-solid border-r-2 border-t-4 border-b-4 border-yellow-500 
       hover:border-transparent cursor-pointer
@@ -31,6 +37,8 @@ function Register() {
           Register as a Buisness
         </span>
       </div>
+
+      {showModal ? <UserModal setShowModal={setShowModal}></UserModal> : null}
     </div>
   );
 }
