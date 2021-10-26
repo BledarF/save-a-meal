16b9d5959f430c3ce3b88bdb03a22eeeba548c75
Sohail <trainee.sohail.saiedi@sigmalabs.zyx>
2021-10-26 15:36:39 +0100

    updated home.js quick

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -8,7 +8,7 @@ import gpPhoto from "./TeamPhotos/Gurpratap.jpg";
 function Home() {
   return (
     <div className="text-2xl">
-      <section className="header flex flex-row items-center justify-center bg-gray-100">
+      <section className="header flex flex-row items-center justify-center ">
         <div className=" header-wrapper flex flex-row justify-center items-center p-5">
           <img
             src="https://images.unsplash.com/photo-1592861956120-e524fc739696?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
