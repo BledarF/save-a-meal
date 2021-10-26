e7a838585967251b596533250ebbedffb219420d
gsinghlak12 <gsinghlak12@gmail.com>
2021-10-26 15:41:26 +0100

    added sessions backend

--- server/Routers/sessions.js
+++ server/Routers/sessions.js
@@ -11,4 +11,56 @@ const pool = new Pool({
 	connectionString: "postgres://localhost:5432/saveameal",
 });
 
+const client = await pool.connect();
+
+router.post("/", async function (req, res) {
+	const { username, password } = await req.body;
+	const sessionID = uuidv4();
+	const userKey = await client.query(`SELECT id FROM users WHERE username=$1`, [
+		username,
+	]);
+
+	await client.query("INSERT INTO sessions (uuid, user_id) VALUES ($1, $2)", [
+		sessionID,
+		userKey.rows[0].id,
+	]);
+
+	res.cookie("sessionID", sessionID).send("cookie sent");
+
+	client.release();
+	console.log("released");
+});
+
+router.get("/cookie", async function (req, res) {
+	const activeSession = await req.cookies;
+	const { user_id } = await req.body;
+	const sessionID = await client.query(
+		`SELECT uuid FROM sessions
+              WHERE user_id=$1`,
+		[user_id]
+	);
+
+	if (sessionID.rows[0]) {
+		if (activeSession.sessionID === sessionID.rows[0].uuid) {
+			res.json({ loggedIn: true });
+			console.log("passes");
+		} else {
+			res.json({ loggedIn: false });
+			console.log("fails");
+		}
+	} else {
+		res.json({ loggedIn: false });
+		console.log("fails");
+	}
+
+	client.release();
+});
+
+router.delete("/", async function () {
+	const activeSession = await req.cookies;
+	const cookieUUID = activeSession.sessionID;
+	client.query(`DELETE FROM sessions WHERE uuid=$1)`, [cookieUUID]);
+	client.release();
+});
+
 module.exports = router;
