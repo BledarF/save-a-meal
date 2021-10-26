7d459a1ffe4e445fe419cf568e478c1fa66b8009
Adnan Gondal <adnangondal1@hotmail.com>
2021-10-26 15:11:51 +0100

    added css for registering

--- client/src/Components/Register/Register.js
+++ client/src/Components/Register/Register.js
@@ -2,12 +2,34 @@ import { useEffect, useState } from "react";
 
 function Register() {
   return (
-    <div className="flex flex-row flex-wrap justify-items-stretch">
-      <div className="flex-auto bg-user-login h-96 bg-cover text-white">
-        Customer
+    <div className="flex flex-row flex-wrap justify-items-stretch my-8 h-screen">
+      <div
+        className="flex-initial bg-user-login  w-1/2 h-5/6 bg-cover  
+      border-solid border-r-2 border-t-4 border-b-4 border-yellow-500 
+      hover:border-transparent cursor-pointer
+      "
+      >
+        <span
+          className="text-white font-bold text-2xl
+        inline-block mt-52
+        "
+        >
+          Register as a Customer
+        </span>
       </div>
-      <div className="flex-auto bg-cafe-register h-96 bg-cover text-white">
-        Buisness
+      <div
+        className="flex-inital  bg-cafe-register bg-opacity-0 w-1/2 h-5/6 bg-cover
+      border-solid border-l-2 border-t-4 border-b-4 border-yellow-500 
+      hover:border-transparent cursor-pointer
+      "
+      >
+        <span
+          className="text-white font-bold text-2xl
+          inline-block mt-52
+        "
+        >
+          Register as a Buisness
+        </span>
       </div>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         "user-login":
           "url('https://images.unsplash.com/photo-1533777857889-4be7c70b33f7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80')",
         "cafe-register":
-          "url('https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1684&q=80')",
+          "url('https://images.unsplash.com/photo-1554118811-1e0d58224f24?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1747&q=80')",
       },
     },
   },
