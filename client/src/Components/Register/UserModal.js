2c40418a2fda62db249fc7c10f813b0295984ebe
Adnan Gondal <adnangondal1@hotmail.com>
2021-10-27 15:57:16 +0100

    made changes to registration page

--- client/src/Components/Register/UserModal.js
+++ client/src/Components/Register/UserModal.js
@@ -8,30 +8,26 @@ function UserModal(props) {
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-auto my-6 mx-auto max-w-6xl">
           {/*content*/}
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          <div className=" pb-8 border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             {/*header*/}
-            <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-              <h3 className="text-3xl font-semibold">Register as Customer</h3>
-              <button className="" onClick={() => props.setShowModal(false)}>
+            <div className="flex items-start justify-between border-b border-solid border-blueGray-200 rounded-t">
+              <h3 className="text-2xl font-semibold p-2 pl-3 pb-1 m-0">
+                Register as Customer
+              </h3>
+              <button
+                className="p-2 pr-5 pb-1"
+                onClick={() => props.setShowModal(false)}
+              >
                 <span className="bg-transparent text-black opacity-50 h-6 w-6 text-2xl block outline-none focus:outline-none">
                   x
                 </span>
               </button>
             </div>
             {/*body*/}
-            <div className="relative p-6 flex-auto">
+            <div className="relative py-1 px-36 flex-auto">
               <UserRegisterForm></UserRegisterForm>
             </div>
             {/*footer*/}
-            <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-              {/* <button
-                className="bg-blue-500 hover:bg-blue-700 transition duration-200 text-white font-bold py-2 px-4 rounded"
-                type="button"
-                onClick={() => props.setShowModal(false)}
-              >
-                Save Changes
-              </button> */}
-            </div>
           </div>
         </div>
       </div>
