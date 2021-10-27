ed65d53b6c06310408272f02d120d00059187694
Adnan Gondal <adnangondal1@hotmail.com>
2021-10-27 15:51:47 +0100

    customer registration done

--- client/src/Components/Register/UserRegisterForm.js
+++ client/src/Components/Register/UserRegisterForm.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { FormikStepper, FormikStep, InputField } from "formik-stepper";
 import passwordValidator from "password-validator";
 
 const schema = new passwordValidator();
@@ -18,139 +20,115 @@ schema
   .not()
   .oneOf(["Passw0rd", "Password123"]); // Blacklist these values
 
-function emailErrorHandle(email, errors) {
-  if (!email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) {
-    errors.email = "Invalid email address";
-  }
-}
-
-function passwordErrorHandle(password, errors) {
-  if (!password) {
-    errors.password = "Required";
-  } else if (!schema.validate(password)) {
-    errors.password = "Invalid password";
-  }
-}
-
-function fieldErrorHandle(values, field, errors) {
-  if (!values[field]) {
-    errors[field] = "Required";
-  }
-}
+const validationSchema = Yup.object().shape({
+  firstName: Yup.string().required("Your first name is required"),
+  lastName: Yup.string().required("A description is required"),
+  email: Yup.string()
+    .email("The email must be a valid email address.")
+    .required("The Email field is required"),
+  password: Yup.string()
+    .required("The Password field is required")
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*)[A-Za-z\d]{6,}$/,
+      `Must Contain 8 Characters, One Uppercase, One Lowercase,
+      One Number and one special case Character [@$!%*#?&-_]`
+    ),
+  streetname: Yup.string().required("The street name is required"),
+  town: Yup.string().required("The town is required"),
+  postcode: Yup.string()
+    .required("The Postcode is required")
+    .matches(
+      "^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$",
+      "Must be a valid UK Postcode"
+    ),
+  telephone: Yup.string().required("Telephone is required"),
+  passwordConfirmation: Yup.string()
+    .required("Password confirmation is required")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
+});
 
 function UserRegisterForm(props) {
+  const onSubmit = async (values, { setSubmitting }) => {
+    console.log(values);
+
+    // FETCH POST REQUEST
+    setError("Error: not connected to the server");
+
+    setSubmitting(false); //// Important
+  };
+
+  const [error, setError] = useState("");
+
   return (
     <div>
-      <Formik
+      <FormikStepper
+        /// Accept all Formik props
+        onSubmit={onSubmit} /// onSubmit Function
         initialValues={{
-          email: "",
-          password: "",
           firstName: "",
           lastName: "",
+          email: "",
+          password: "",
+          privacy: false,
+          description: "",
+          town: "",
+          postcode: "",
+          streetname: "",
+          telephone: "",
+          passwordConfirmation: "",
         }}
-        validate={(values) => {
-          const errors = {};
-
-          emailErrorHandle(values.email, errors);
-          passwordErrorHandle(values.password, errors);
-          fieldErrorHandle(values, "firstName", errors);
-          fieldErrorHandle(values, "lastName", errors);
-
-          return errors;
-        }}
-        onSubmit={async (values, { setSubmitting }) => {
-          await new Promise((r) => setTimeout(r, 500));
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }}
+        validationSchema={validationSchema}
+        labelsColor="secondary" /// The text label color can be root variables or css => #fff
+        withStepperLine /// false as default and If it is false, it hides stepper line
+        nextBtnLabel="Next" /// Next as default
+        prevBtnLabel="Back" /// Prev as default
+        submitBtnLabel="Submit" /// Submit as default
+        nextBtnColor="warning" /// as default and The color can be root variables or css => #fff
+        prevBtnColor="danger" /// as default and The color can be root variables or css => #fff
+        submitBtnColor="success" /// as default and The color can be root variables or css => #fff
       >
-        {({ isSubmitting }) => (
-          <Form>
-            <div className="flex">
-              <div>
-                <div className="flex-auto flex flex-row p-2">
-                  <label htmlFor="email" className="p-2">
-                    Email
-                  </label>
-                  <Field
-                    className=" p-2"
-                    id="email"
-                    name="email"
-                    placeholder="jane@acme.com"
-                    type="email"
-                  />
-                </div>
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="text-red-500"
-                />
-                <div className="flex-auto p-2">
-                  <label htmlFor="firstName" className="p-2">
-                    First Name
-                  </label>
-                  <Field
-                    id="firstName"
-                    name="firstName"
-                    placeholder="Jana"
-                    className="p-2"
-                  />
-                </div>
-                <ErrorMessage
-                  name="firstName"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-              <div>
-                <div className="flex-auto p-2">
-                  <label htmlFor="password" className="p-2">
-                    Password
-                  </label>
-                  <Field
-                    id="password"
-                    name="password"
-                    placeholder=""
-                    className="p-2"
-                  />
-                </div>
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  className="text-red-500"
-                />
+        {/*  First Step */}
+        <FormikStep
+          label="Personal Information" /// The text label of Step
+          withIcons="fa fa-user" // to add icon into the circle must add icon as class Name like Fontawesome
+          withNumbers /// If true, it hides the icon and shows the step number
+          iconColor="white" /// The color can be root variables or css => #fff
+          circleColor="#F59E0B" /// The color can be root variables or css => #fff
+        >
+          <InputField name="firstName" label="First Name" />
+          <InputField name="lastName" label="Last Name" />
+          <div className="flex">
+            <InputField
+              className="mx-2"
+              name="streetname"
+              label="Street Name"
+            />
+            <InputField className="mx-2" name="town" label="Town" />
+          </div>
 
-                <div className="flex-auto p-2">
-                  <label htmlFor="lastName" className="p-2">
-                    Last Name
-                  </label>
-                  <Field
-                    id="lastName"
-                    name="lastName"
-                    placeholder="Doe"
-                    className="p-2"
-                  />
-                </div>
-                <ErrorMessage
-                  name="lastName"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-            </div>
+          <div className="flex">
+            <InputField className="mx-2" name="postcode" label="Postcode" />
+            <InputField className="mx-2" name="telephone" label="Telephone" />
+          </div>
+        </FormikStep>
 
-            <button
-              className="bg-yellow-500 hover:bg-blue-700 transition duration-200 text-white font-bold py-2 px-4 rounded mt-12"
-              type="submit"
-              disabled={isSubmitting}
-            >
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
+        {/* Third Step */}
+        <FormikStep
+          label="Login Info"
+          withIcons="fa fa-lock"
+          iconColor="white"
+          circleColor="#F59E0B"
+        >
+          <InputField name="email" label="Email" type="email" />
+          <InputField name="password" label="Password" type="password" />
+          <InputField
+            name="passwordConfirmation"
+            label="Confirm Password"
+            type="password"
+          />
+        </FormikStep>
+      </FormikStepper>
+      <div className="text-red-500">{error}</div>
     </div>
   );
 }
