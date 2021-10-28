fc8eb6c5cb6430e174bcbbabbd1bc8929a57947f
Adnan Gondal <adnangondal1@hotmail.com>
2021-10-28 13:33:08 +0100

    added account page

--- client/src/Components/Account/BusinessAccount.js
+++ client/src/Components/Account/BusinessAccount.js
@@ -67,9 +67,28 @@ const Tabs = ({ color }) => {
                 Orders
               </a>
             </li>
+            <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+              <a
+                className={
+                  "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                  (openTab === 4
+                    ? "text-white bg-" + color + "-600"
+                    : "text-" + color + "-600 bg-white")
+                }
+                onClick={(e) => {
+                  e.preventDefault();
+                  setOpenTab(4);
+                }}
+                data-toggle="tab"
+                href="#link4"
+                role="tablist"
+              >
+                Availability
+              </a>
+            </li>
           </ul>
           <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
-            <div className="px-4 py-5 flex-auto">
+            <div className="px-4 py-5 ">
               <div className="tab-content tab-space">
                 <div className={openTab === 1 ? "block" : "hidden"} id="link1">
                   <p>
@@ -97,6 +116,14 @@ const Tabs = ({ color }) => {
                     Efficiently unleash cross-media information without
                     cross-media value. Quickly maximize timely deliverables for
                     real-time schemas.
+                    <br />
+                    <br /> Dramatically maintain clicks-and-mortar solutions
+                    without functional solutions.
+                  </p>
+                </div>
+                <div className={openTab === 4 ? "block" : "hidden"} id="link4">
+                  <p>
+                    I have no idea what I'm doing lol
                     <br />
                     <br /> Dramatically maintain clicks-and-mortar solutions
                     without functional solutions.
