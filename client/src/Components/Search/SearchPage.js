23170e808a2217cf74c26b6c2cf99f80c76e2098
gsinghlak12 <gsinghlak12@gmail.com>
2021-10-29 10:30:42 +0100

    minor changes to backend

--- client/src/Components/Search/SearchPage.js
+++ client/src/Components/Search/SearchPage.js
@@ -5,24 +5,22 @@ import "./SearchPage.css";
 import { add } from "date-fns";
 
 function SearchPage() {
+	// this is a test for git
+	const [userPostcodeInput, setUserPostcodeInput] = useState("");
 
-  // this is a test for git
-  const [userPostcodeInput, setUserPostcodeInput] = useState("");
+	// get seatch input
+	// send in body of get fetch
+	// get
 
-  // get seatch input
-  // send in body of get fetch
-  // get
-
-  return (
-    <div className="search-page-wrapper flex flex-col">
-      <div className="pt-10 flex items-center justify-center">
-        <SearchBar setUserPostcodeInput={setUserPostcodeInput} />
-      </div>
-      <div className="flex flex-row">
-        <Results />
-      </div>
-    </div>
-  );
-
-
+	return (
+		<div className="search-page-wrapper flex flex-col">
+			<div className="pt-10 flex items-center justify-center">
+				<SearchBar setUserPostcodeInput={setUserPostcodeInput} />
+			</div>
+			<div className="flex flex-row">
+				<Results />
+			</div>
+		</div>
+	);
+}
 export default SearchPage;
