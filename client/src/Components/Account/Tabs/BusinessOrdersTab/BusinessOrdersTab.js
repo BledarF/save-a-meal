be345064902bc41c6743fe4c0ec5f12290111c0c
Adnan Gondal <adnangondal1@hotmail.com>
2021-10-29 11:54:51 +0100

    added tabs, seperated into components

---
+++ client/src/Components/Account/Tabs/BusinessOrdersTab/BusinessOrdersTab.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import BusinessOrderEntry from "./BusinessOrderEntry";
+import TodayTable from "./TodayTable";
+
+function BusinessOrdersTab(props) {
+  useEffect(() => {
+    //FETCH ORDERS DATA
+  }, []);
+
+  function getTodayTable() {
+    return <TodayTable></TodayTable>;
+  }
+
+  return (
+    <div className="flex flex-col text-left">
+      <div>
+        <h4>Today</h4>
+        <p>No orders Today</p>
+        {getTodayTable()}
+      </div>
+      <div>
+        <h4>History</h4>
+        <p>You have had no orders</p>
+      </div>
+    </div>
+  );
+}
+
+export default BusinessOrdersTab;
---
+++ client/src/Components/Account/Tabs/BusinessOrdersTab/TodayTable.js
@@ -0,0 +1,35 @@
+import React from "react";
+import BusinessOrderEntry from "./BusinessOrderEntry";
+
+function TodayTable(props) {
+  return (
+    <div className="flex justify-center m-0">
+      <div className="flex flex-col">
+        <div className="w-full">
+          <div className="border-b border-gray-200 shadow">
+            <table>
+              <thead className="bg-gray-50">
+                <tr>
+                  <th className="px-6 py-2 text-xs text-gray-500">ID</th>
+                  <th className="px-6 py-2 text-xs text-gray-500">Name</th>
+                  <th className="px-6 py-2 text-xs text-gray-500">Telephone</th>
+                  <th className="px-6 py-2 text-xs text-gray-500">
+                    Created_at
+                  </th>
+                  <th className="px-6 py-2 text-xs text-gray-500">Status</th>
+                  <th className="px-6 py-2 text-xs text-gray-500">Delete</th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                {/* Map an Orders State here... */}
+                <BusinessOrderEntry accepted={false}></BusinessOrderEntry>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodayTable;
