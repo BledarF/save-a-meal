e3bceb26996a356bba382950b8b0b5cd8a7188c7
Adnan Gondal <adnangondal1@hotmail.com>
2021-10-29 15:03:32 +0100

    get user data

--- client/src/Components/Account/Account.js
+++ client/src/Components/Account/Account.js
@@ -1,8 +1,32 @@
-import { useEffect, useContext, useState } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import UserAccount from "./userAccount";
 import BusinessAccount from "./BusinessAccount";
+import { userContext } from "../../App";
 
 function Account() {
+  const { user, setUser } = useContext(userContext);
+
+  async function fetchAccount() {
+    console.log(user.id);
+    try {
+      const res = await fetch(`http://localhost:8080/api/users/`, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // body: JSON.stringify(values),
+      });
+      const data = await res.json();
+      console.log(data);
+    } catch {
+      //alert("Error");
+    }
+  }
+
+  React.useEffect(() => {
+    fetchAccount();
+  }, []);
   return (
     <>
       <div className="flex justify-center">
