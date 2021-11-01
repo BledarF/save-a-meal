eab671b4db3e74c033b8b304fa35c18db4878267
Sohail <trainee.sohail.saiedi@sigmalabs.zyx>
2021-11-01 16:17:50 +0000

    animated header background

--- client/src/Components/Account/Tabs/AccountTab.js
+++ client/src/Components/Account/Tabs/AccountTab.js
@@ -1,4 +1,4 @@
-import { tr } from "date-fns/locale";
+// import { tr } from "date-fns/locale";
 import React, { useState } from "react";
 
 function AccountTab(props) {
@@ -54,7 +54,10 @@ function AccountTab(props) {
           <div className="table-row">
             <div className="table-cell pb-1">Email: </div>
             {editMode ? (
-              <input onChange={(e) => setEmail(e.target.value)} placeholder={props.email}></input>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder={props.email}
+              ></input>
             ) : (
               <div className="table-cell">{email ? email : props.email}</div>
             )}
@@ -62,15 +65,25 @@ function AccountTab(props) {
           <div class="table-row">
             <div class="table-cell pb-1">Telephone: </div>
             {editMode ? (
-              <input type="tel" onChange={(e) => setTelephone(e.target.value)} placeholder={props.telephone}></input>
+              <input
+                type="tel"
+                onChange={(e) => setTelephone(e.target.value)}
+                placeholder={props.telephone}
+              ></input>
             ) : (
-              <div className="table-cell">{telephone ? telephone : props.telephone}</div>
+              <div className="table-cell">
+                {telephone ? telephone : props.telephone}
+              </div>
             )}
           </div>
           <div className="table-row">
             <div className="table-cell pb-1">Password: </div>
             {editMode ? (
-              <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder={"Enter new password"}></input>
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder={"Enter new password"}
+              ></input>
             ) : (
               <div className="table-cell">****************</div>
             )}
@@ -79,15 +92,24 @@ function AccountTab(props) {
       </div>
       {editMode ? (
         <div>
-          <button onClick={handleEdit} className="bg-green-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full ">
+          <button
+            onClick={handleEdit}
+            className="bg-green-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full "
+          >
             Save
           </button>
-          <button onClick={editToggle} className="bg-red-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full ">
+          <button
+            onClick={editToggle}
+            className="bg-red-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full "
+          >
             Cancel
           </button>
         </div>
       ) : (
-        <button onClick={editToggle} className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full ">
+        <button
+          onClick={editToggle}
+          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full "
+        >
           Edit
         </button>
       )}
