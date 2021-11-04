966b6d8e3c5a4fd920ba1f6fb1512b9aba905224
Sohail <trainee.sohail.saiedi@sigmalabs.zyx>
2021-11-04 17:56:29 +0000

    finished about us

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,12 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import {
-	Route,
-	BrowserRouter as Router,
-	useHistory,
-	Switch,
-	Redirect,
-	NavLink,
+  Route,
+  BrowserRouter as Router,
+  useHistory,
+  Switch,
+  Redirect,
+  NavLink,
 } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
@@ -16,6 +16,7 @@ import Register from "./Components/Register/Register";
 import SearchPage from "./Components/Search/SearchPage";
 import Account from "./Components/Account/Account";
 import ErrorPage from "./Components/ErrorPage";
+import About from "./Components/About/About";
 
 // CONTEXT API
 
@@ -32,13 +33,12 @@ export const bookingContext = React.createContext({
 });
 
 function App(props) {
-
-	const [data, setData] = useState(null);
-	const [sessionUpdate, setSessionUpdate] = useState(0);
-	const history = useHistory;
-	// CONTEXT API
-	const [user, setUser] = useState("");
-	const value = { user, setUser };
+  const [data, setData] = useState(null);
+  const [sessionUpdate, setSessionUpdate] = useState(0);
+  const history = useHistory;
+  // CONTEXT API
+  const [user, setUser] = useState("");
+  const value = { user, setUser };
 
   useEffect(() => {
     if (sessionUpdate == 0) {
@@ -67,28 +67,27 @@ function App(props) {
     }
   }
 
+  return (
+    <userContext.Provider value={value}>
+      <div className="App">
+        <Router>
+          <Navbar checkSessionExists={checkSessionExists} />
+          <main>
+            <Switch>
+              <Route exact path={["/home", "/"]} component={Home} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/search" component={SearchPage} />
+              <Route exact path="/account" component={Account} />
+              <Route exact path="/404" component={ErrorPage} />
+              <Redirect exact from="*" to="/404" />
+            </Switch>
+          </main>
 
-	return (
-		<userContext.Provider value={value}>
-			<div className="App">
-				<Router>
-					<Navbar checkSessionExists={checkSessionExists} />
-					<main>
-						<Switch>
-							<Route exact path={["/home", "/"]} component={Home} />
-							<Route exact path="/register" component={Register} />
-							<Route exact path="/search" component={SearchPage} />
-							<Route exact path="/account" component={Account} />
-							<Route path="/404" component={ErrorPage} />
-							<Redirect exact from="*" to="/404" />
-						</Switch>
-					</main>
-
-					<Footer />
-				</Router>
-			</div>
-		</userContext.Provider>
-	);
+          <Footer />
+        </Router>
+      </div>
+    </userContext.Provider>
+  );
 }
 
 export default App;
