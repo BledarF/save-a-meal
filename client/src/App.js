c89d423ed2c53058e9efd7487359b199a164f28b
gsinghlak12 <gsinghlak12@gmail.com>
2021-11-04 13:52:10 +0000

    error page in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,12 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import {
-  Route,
-  BrowserRouter as Router,
-  useHistory,
-  Switch,
-  Redirect,
-  NavLink,
+	Route,
+	BrowserRouter as Router,
+	useHistory,
+	Switch,
+	Redirect,
+	NavLink,
 } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
@@ -20,86 +20,88 @@ import ErrorPage from "./Components/ErrorPage";
 // CONTEXT API
 
 export const userContext = React.createContext({
-  // MAYBE GET FROM COOKIES INITIALLY?
-  user: null,
-  setUser: () => {},
+	// MAYBE GET FROM COOKIES INITIALLY?
+	user: null,
+	setUser: () => {},
 });
 
 export const bookingContext = React.createContext({
-  // MAYBE GET FROM COOKIES INITIALLY?
-  bookingStatus: true,
-  setBookingStatus: () => {},
+	// MAYBE GET FROM COOKIES INITIALLY?
+	bookingStatus: true,
+	setBookingStatus: () => {},
 });
 
 function App(props) {
-  const [data, setData] = useState(null);
-  const [sessionUpdate, setSessionUpdate] = useState(0);
-  const history = useHistory;
-  // CONTEXT API
-  const [user, setUser] = useState("");
-  const value = { user, setUser };
+	const [data, setData] = useState(null);
+	const [sessionUpdate, setSessionUpdate] = useState(0);
+	const history = useHistory;
+	// CONTEXT API
+	const [user, setUser] = useState("");
+	const value = { user, setUser };
 
-  useEffect(() => {
-    if (sessionUpdate == 0) {
-      checkSessionExists();
-    }
-    updateCurrentSlotsMidnight();
-  });
+	useEffect(() => {
+		if (sessionUpdate == 0) {
+			checkSessionExists();
+		}
+		updateCurrentSlotsMidnight();
+	});
 
-  async function checkSessionExists() {
-    setSessionUpdate(1);
-    try {
-      const response = await fetch(`http://localhost:8080/api/sessions/check`, {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        // body: JSON.stringify(values),
-      });
-      const jsonResponse = await response.json();
-      //console.log(jsonResponse);
-      if (jsonResponse.id) {
-        setUser(jsonResponse.id);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	async function checkSessionExists() {
+		setSessionUpdate(1);
+		try {
+			const response = await fetch(`http://localhost:8080/api/sessions/check`, {
+				method: "GET",
+				credentials: "include",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				// body: JSON.stringify(values),
+			});
+			const jsonResponse = await response.json();
+			//console.log(jsonResponse);
+			if (jsonResponse.id) {
+				setUser(jsonResponse.id);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  async function updateCurrentSlotsMidnight() {
-    const today = new Date();
-    const currentTime =
-      today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	async function updateCurrentSlotsMidnight() {
+		const today = new Date();
+		const currentTime =
+			today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-    if (currentTime === "00:00:00") {
-      await fetch("http://localhost:8080/api/restaurants/reset", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    }
-  }
+		if (currentTime === "00:00:00") {
+			await fetch("http://localhost:8080/api/restaurants/reset", {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+		}
+	}
 
-  return (
-    <userContext.Provider value={value}>
-      <div className="App">
-        <Navbar checkSessionExists={checkSessionExists} />
-        <Router>
-          <main>
-            <Switch>
-              <Route exact path={["/home", "/"]} component={Home} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/search" component={SearchPage} />
-              <Route exact path="/account" component={Account} />
-            </Switch>
-          </main>
-        </Router>
-        <Footer />
-      </div>
-    </userContext.Provider>
-  );
+	return (
+		<userContext.Provider value={value}>
+			<div className="App">
+				<Navbar checkSessionExists={checkSessionExists} />
+				<Router>
+					<main>
+						<Switch>
+							<Route exact path={["/home", "/"]} component={Home} />
+							<Route exact path="/register" component={Register} />
+							<Route exact path="/search" component={SearchPage} />
+							<Route exact path="/account" component={Account} />
+							<Route exact path="/404" component={ErrorPage} />
+							<Redirect from="*" to="/404" />
+						</Switch>
+					</main>
+				</Router>
+				<Footer />
+			</div>
+		</userContext.Provider>
+	);
 }
 
 export default App;
