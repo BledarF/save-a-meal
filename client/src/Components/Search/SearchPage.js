a79fd7dfbfe27ae2ec8353c035a1383c689b4a46
Sohail <trainee.sohail.saiedi@sigmalabs.zyx>
2021-11-05 12:46:52 +0000

    centered div :)

--- client/src/Components/Search/SearchPage.js
+++ client/src/Components/Search/SearchPage.js
@@ -4,29 +4,29 @@ import Results from "./Results";
 import "./SearchPage.css";
 
 function SearchPage() {
-	const [restaurants, setRestaurants] = useState([]);
-	const [bookingStatus, setBookingStatus] = useState(true);
-	const [searchStatus, setSearchStatus] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
+  const [bookingStatus, setBookingStatus] = useState(true);
+  const [searchStatus, setSearchStatus] = useState("");
 
-	return (
-		<div className="search-page-wrapper flex flex-col">
-			<div className="pt-10 flex items-center justify-center">
-				<SearchBar
-					setRestaurants={setRestaurants}
-					setBookingStatus={setBookingStatus}
-					setSearchStatus={setSearchStatus}
-				/>
-			</div>
-			<div className="flex flex-row">
-				<Results
-					restaurants={restaurants}
-					bookingStatus={bookingStatus}
-					setBookingStatus={setBookingStatus}
-					searchStatus={searchStatus}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="search-page-wrapper flex flex-col">
+      <div className="pt-10 flex items-center justify-center">
+        <SearchBar
+          setRestaurants={setRestaurants}
+          setBookingStatus={setBookingStatus}
+          setSearchStatus={setSearchStatus}
+        />
+      </div>
+      <div className="flex flex-row justify-center">
+        <Results
+          restaurants={restaurants}
+          bookingStatus={bookingStatus}
+          setBookingStatus={setBookingStatus}
+          searchStatus={searchStatus}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default SearchPage;
