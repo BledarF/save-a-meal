74680e0395ce316570f46c79c6bc5d99bc03f2e6
Sohail <trainee.sohail.saiedi@sigmalabs.zyx>
2021-11-05 11:53:36 +0000

    beautified restaurant modal and card animations

--- client/src/Components/Search/RestaurantModal/RestaurantModalHeader.js
+++ client/src/Components/Search/RestaurantModal/RestaurantModalHeader.js
@@ -1,16 +1,10 @@
-import React, { useEffect, useState, useContext } from "react";
-import { userContext } from "../../../App";
-
 function RestaurantModalHeader(props) {
-  console.log(props);
   const { logo, name, town } = props.headerDetails;
-
-  const { user, setUser } = useContext(userContext);
   const { setShowModal } = props;
 
   return (
-    <div className="flex items-center p-3 justify-around border-b border-solid border-blueGray-200 rounded-t">
-      <img className="w-14" src={logo} alt={`${name} Logo`} />
+    <div className="flex items-center p-3 justify-between border-b border-solid border-blueGray-200 rounded-t">
+      <img className=" h-20" src={logo} alt={`${name} Logo`} />
       <h3 className="text-2xl font-semibold">
         {name} - {town}
       </h3>
@@ -20,7 +14,7 @@ function RestaurantModalHeader(props) {
           setShowModal(false);
         }}
       >
-        <span className="bg-transparent text-black opacity-50 h-6 w-6 text-2xl block outline-none focus:outline-none">
+        <span className="bg-transparent text-red-600 opacity-50 h-6 w-6 text-2xl block outline-none focus:outline-none mr-3">
           &#10006;
         </span>
       </button>
